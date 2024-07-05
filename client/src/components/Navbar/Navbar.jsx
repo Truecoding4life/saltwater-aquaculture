da55bfdace4e13d132f72e70dfd37be3e1aa3c21
Master <nghiemxthai@gmail.com>
2024-07-05 16:54:26 -0700

    Added Search on Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import {Container, Row, Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import { Container, Button, Row, Nav, Navbar, NavDropdown, Form, Col } from 'react-bootstrap'
 import "./style.css"
 const NavbarCompt = () => {
-    return (
-        <Navbar expand="lg"   className="bg-body-tertiary fixed-top">
-      <Container id='navbar'>
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+      <Container id='navbar' className='justify-content-between'>
         <Navbar.Brand href="#home"> Live Aquarium Fish</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto " >
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Sale</Nav.Link>
             <NavDropdown title="Sale" id="basic-nav-dropdown">
@@ -21,11 +21,30 @@ const NavbarCompt = () => {
                 Contact Us
               </NavDropdown.Item>
             </NavDropdown>
+
           </Nav>
+            <Form inline>
+              <Row>
+                <Col xs='auto'>
+                 <Form.Control
+                  placeholder="Search"
+                  id='search'
+                  aria-label="Username"
+                  aria-describedby="basic-addon1">
+
+                  </Form.Control>
+                </Col>
+                <Col xs='auto'>
+                  <Button type='success' variant="success">Search</Button>
+                </Col>
+              </Row>
+               
+             
+            </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
+  )
 }
 
 export default NavbarCompt
